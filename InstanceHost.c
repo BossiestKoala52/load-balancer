b00f165ed75e1f840eeac0c176de8555dc5bb8d9
Nick Jones <ndjone12@asu.edu>
2023-09-13 13:38:59 -0500

    init

--- InstanceHost.c
+++ InstanceHost.c
@@ -7,6 +7,7 @@
 //struct for representing the host
 struct host{
     struct job_node* batch;
+    pthread_mutex_t host_lock;
 };
 
 struct job_node; //defined in LoadBalancer
@@ -26,7 +27,14 @@ host* host_create(){
 * completed.
 */
 void host_destroy(host** host){
-
+    if(host[0]->batch != NULL){
+        struct job_node* current = host[0]->batch;
+        while(current != NULL){
+            struct job_node* next = current->next;
+            *current->data_result = (current->data * current->data);
+            current = next;
+        }
+    }
 }
 
 /**
@@ -47,6 +55,6 @@ void host_request_instance(host* h, struct job_node* batch){
         current = next;
     }
     free(current);
-
+    host_destroy(& h);
 }
 
